40bfaed0a3efec089a1cb3196623077498d0b2b6
Hamster-zc <1683423386@qq.com>
2026-06-10 11:19:40 +0800

    Week2 Day2 finish test

--- test/test_llm.py
+++ test/test_llm.py
@@ -1,7 +1,10 @@
+import sys, os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from node.llm_wrapper import LLMWrapper
 
 def test_llm_wrapper(model_path):
-    llm = LLMWrapper(model_path=model_path, max_tokens=128, device='cpu')
+    llm = LLMWrapper(model_path=model_path, max_tokens=128, device='cuda')
     prompt = "What is the capital of France?"
     response = llm.generate(prompt)
     print("Response:", response)
@@ -9,3 +12,6 @@ def test_llm_wrapper(model_path):
     assert len(response['choices']) > 0
     assert 'text' in response['choices'][0]
     llm.close()
+
+if __name__ == "__main__":
+    test_llm_wrapper(r"model\Qwen2.5-7B-Instruct-Q4_K_M.gguf")
